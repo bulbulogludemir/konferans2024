38759aad1976b01c3865306e3c64035553fda058
Demir Bülbüloğlu <demirbulbuloglu61@gmail.com>
2024-05-06 13:17:45 +0300

    Update geri-sayim.js

--- geri-sayim.js
+++ geri-sayim.js
@@ -1,8 +1,14 @@
-const targetDate = new Date("2024-05-10");
+const targetDate = new Date("2024-05-10T00:00:00");
 
 const updateCountdown = () => {
   const now = new Date();
   const diff = targetDate - now;
+
+  if (diff <= 0) {
+    clearInterval(interval);
+    document.querySelector(".geri-sayim h2").textContent = "Konferans Başladı!";
+    return;
+  }
 
   const days = Math.floor(diff / (1000 * 60 * 60 * 24));
   const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
@@ -13,11 +19,6 @@ const updateCountdown = () => {
   document.querySelector(".saatler span").textContent = hours;
   document.querySelector(".dakikalar span").textContent = minutes;
   document.querySelector(".saniyeler span").textContent = seconds;
-
-  if (diff <= 0) {
-    clearInterval(interval);
-    document.querySelector(".geri-sayim h2").textContent = "Konferans Başladı!";
-  }
 };
 
 const interval = setInterval(updateCountdown, 1000);
